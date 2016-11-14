fd4907e6270993911b62443a13b484c0a63c0b20
Dan Murphy <danielmarkmurphy@gmail.com>
2016-11-13 17:40:06 -0800

    renderDataTable options no needed
    because object being rendered is now a datatable object

--- server.r
+++ server.r
@@ -67,7 +67,7 @@ shinyServer(function(input, output, session) {
     if (is.null(rval$df)) return(NULL)
     if (!input$showtable) return(NULL)
     DT::datatable(rval$df, options = list(pageLength = 5), rownames = TRUE)
-  }, options = list(lengthMenu = c(5, 20, 50), pageLength = 5))
+  })
 
   output$tableplot <- renderPlot({
     # If no table yet, nothing to plot
